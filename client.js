fbc7405c807c1467b0414f6bc191b6bb15b753ba
evantilton <evantilton@gmail.com>
2020-04-21 11:15:17 -0500

    hey, got it to work,  got it to display a random item on button press

--- client.js
+++ client.js
@@ -1,12 +1,25 @@
 console.log('Jquery is ready')
 
+let array = ["item zero","item one", "item two", "item three", "item four"];
+let item;
 
 let displayInput = 
 $(document).ready(onReady);
 
 function onReady() {
     console.log('in onReady');
-    getMath();
     $('#generateButton').on('click', generateFunction);
 }//end onReady
+
+
+function generateFunction() {
+    console.log(array[0]);
+    let number = (Math.floor(Math.random() * array.length));
+    $('#randomitem').remove();
+    $('#randomItem').append(`
+    <div>${(array[number])}</div>
+`
+    )
+}
+
 
